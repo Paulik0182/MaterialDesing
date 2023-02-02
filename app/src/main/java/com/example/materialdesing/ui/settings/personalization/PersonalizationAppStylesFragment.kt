0afc4e9dfb740eb959073de7da156e4298c22797
Paul <paulik0182@gmail.com>
2023-02-02 14:00:21 +0300

    Перемешивание элементов

--- app/src/main/java/com/example/materialdesing/ui/settings/personalization/PersonalizationAppStylesFragment.kt
+++ app/src/main/java/com/example/materialdesing/ui/settings/personalization/PersonalizationAppStylesFragment.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import androidx.transition.TransitionManager
 import com.example.materialdesing.R
 import com.example.materialdesing.databinding.FragmentPersonalizationAppStylesBinding
+import com.google.android.material.chip.Chip
 
 class PersonalizationAppStylesFragment : Fragment(R.layout.fragment_personalization_app_styles) {
 
     private var _binding: FragmentPersonalizationAppStylesBinding? = null
     private val binding get() = _binding!!
+    var isFlag = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,10 +43,38 @@ class PersonalizationAppStylesFragment : Fragment(R.layout.fragment_personalizat
     private fun clickThemeChipGroup() {
         binding.themeChipGroup.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
-                R.id.pink_theme_chip -> requireContext().setTheme(R.style.PinkTheme)
-                R.id.green_theme_chip -> requireContext().setTheme(R.style.GreenTheme)
-                R.id.white_theme_chip -> requireContext().setTheme(R.style.WhiteTheme)
+                R.id.pink_theme_chip -> {
+                    requireContext().setTheme(R.style.PinkTheme)
+                    mixingElementsChipGroup()
+                }
+                R.id.green_theme_chip -> {
+                    requireContext().setTheme(R.style.GreenTheme)
+                    mixingElementsChipGroup()
+                }
+                R.id.white_theme_chip -> {
+                    requireContext().setTheme(R.style.WhiteTheme)
+                    mixingElementsChipGroup()
+                }
             }
+        }
+    }
+
+    private fun mixingElementsChipGroup() {
+        val chipId: List<Int> =
+            listOf((R.id.pink_theme_chip), (R.id.green_theme_chip), (R.id.white_theme_chip))
+
+        TransitionManager.beginDelayedTransition(binding.root)
+        binding.themeChipGroup.removeAllViews()
+        chipId.shuffled().random()
+        chipId.forEach {
+            binding.themeChipGroup.addView(Chip(requireContext()).apply {
+                id = it
+                when (id) {
+                    chipId[0] -> text = getText(R.string.pink_theme)
+                    chipId[1] -> text = getText(R.string.green_theme)
+                    chipId[2] -> text = getText(R.string.white_theme)
+                }
+            })
         }
     }
 
